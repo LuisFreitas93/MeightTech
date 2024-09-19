b7d7233f532e18dd228a3cd5f1e7754c8c3d096a
Luis Freitas <luisazevedo93@hotmail.com>
2024-09-19 10:26:39 +0100

    Initial commit

--- ETL pipeline.py
+++ ETL pipeline.py
@@ -3,7 +3,9 @@ import numpy as np
 import snowflake.connector
 import snowflake.connector.pandas_tools as pd_tools
 
-###### EXTRACT ######
+
+
+#################################### EXTRACT ####################################
 # 1 . Load CSV data for testing and first import
 #df = pd.read_csv('/Users/luis.freitas/Desktop/Workspace/challenge_dataset.csv')
 
@@ -21,16 +23,20 @@ data = {
 }
 
 df = pd.DataFrame(data)
-###### TRANSFORM ######
+
+
+
+#################################### TRANSFORM ####################################
 # 1. Clean service_type
 df = df.replace('full_truck_lod', 'full_truck_load')
-
 
 # 2. Drop missing values (any column)
 df = df.dropna()
 
 # 3. Apply IQR
-#The IQR is the range between the first quartile (25th percentile) and the third quartile (75th percentile) in a dataset. Data points that fall outside of 1.5 times the IQR below the first quartile or above the third quartile are often considered outliers.
+# The IQR is the range between the first quartile (25th percentile) and the third quartile (75th percentile) in a dataset. 
+# Data points that fall outside of 1.5 times the IQR below the first quartile or above the third quartile are often considered outliers.
+
 # Calculate Q1 (25th percentile) and Q3 (75th percentile)
 Q1 = df['price'].quantile(0.25)
 Q3 = df['price'].quantile(0.75)
@@ -42,7 +48,10 @@ upper_bound = Q3 + 1.5 * IQR
 
 # Filter out outliers
 df = df[(df['price'] >= lower_bound) & (df['price'] <= upper_bound)]
-###### LOAD ######
+
+
+
+#################################### LOAD ####################################
 
 # 1. Define Snowflake connection details
 conn = snowflake.connector.connect(
@@ -54,13 +63,13 @@ conn = snowflake.connector.connect(
     schema='PUBLIC'
 )
 
-# Step 3: Create a cursor object to interact with Snowflake
+# 2. Create a cursor object to interact with Snowflake
 cur = conn.cursor()
 
-# Step 5: Ensure DataFrame columns match the Snowflake table
+# 3. Ensure DataFrame columns match the Snowflake table
 df = df[['service_type', 'pickup_coordinate', 'delivery_coordinate', 'price', 'trailer_type']]
 
-# Step 6: Load the DataFrame into Snowflake
+# 4. Load the DataFrame into Snowflake (append)
 success, nchunks, nrows, _ = pd_tools.write_pandas(conn, df, 'FREIGHT_DATA')
 
 # Check if the data load was successful
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # MeightTech
+
+
+Meight Road Freight Price Analytics System
+
+Scenario:
+
+Meight tracks market prices for road freight services such as full-truck-load, less-than-truck-load, refrigerated services, and flatbeds across various European regions. The aim is to develop a system that computes analytics based on historical data to offer insights into road freight pricing.
+
+Objective:
+
+The project involves designing and building a data pipeline that processes and transforms historical road freight market prices. The system should enable analytics output filtered by pickup_region, delivery_region, season, and service_type, providing insights based on a substantial volume of records.
+
+Challenge Steps:
+
+	1.	Data Pipeline Scheduling and Automation:
+	•	Objective: Automate the ETL pipeline using tools like Apache Airflow to ensure data is processed and updated on a regular schedule (daily or weekly).
+	2.	Analytics Integration:
+	•	Objective: Generate analytics that return the 25th percentile, 75th percentile, and average of the user-selected input data.
+	3.	Data Visualization and Reporting (in Progress):
+	•	Objective: Provide visualizations for historical price trends by region and service type. Include forecasted prices and their comparison with actual prices, moving averages, price fluctuations, and market trends.
+
+Setup and Installation
+
+    For the ETL pipeline
+	•	Clone the repository: git clone https://github.com/LuisFreitas93/MeightTech.git
+	•	Install dependencies: pip install -r requirements.txt
+	•	Run the ETL pipeline 
+
+    For Analytics
+    •	Go to Snowflake https://app.snowflake.com/tykejnb/aa50676/w5OXb6P3KEeB#query with credentials:
+        user='LFREITASTRIAL',
+        password='MeightTest1',
+    •	Go to page and run the function with arguments
+            road_freight_analytics_v1(service_type, trailer_type, pickup_coordinate, delivery_coordinate);
+
+
+    For Scheduling (in progress)
+
+    For Reporting (in progress)
